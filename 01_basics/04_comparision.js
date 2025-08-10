6e7140bdcc32c1da7fe0dd1826c1dfd56f258c84
Md Ahmed <mdqamarahmed123@gmail.com>
2025-08-10 07:56:28 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log("1"<0); // It will check with the value
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+console.log("2"===2); // === will check the datatype so its false
+
