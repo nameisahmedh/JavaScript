408d3ead4dcc461525f04d96005b7c6e3941d95d
Md Ahmed <mdqamarahmed123@gmail.com>
2025-08-10 11:51:16 +0000

    Numbers and Math

---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,46 @@
+// ----------> Numbers
+const num_age = 50
+// console.log(num_age);
+
+const amount = new Number(1500)
+// console.log(amount);
+// console.log(amount.toString().length);
+// console.log(amount.toFixed(1));
+
+
+const otherNumber = 123.8967
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundrends = 1000000000
+// console.log(hundrends.toLocaleString('en-IN'));
+
+
+// -------> Maths
+
+console.log(Math);
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.4));
+// console.log(Math.PI);
+// console.log(Math.max(5,6,9));
+// console.log(Math.min(5,6,9));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min = 50;
+const max = 60;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
