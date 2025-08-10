0882421d485ee717b5bef31a52d62f72a2276f70
Md Ahmed <mdqamarahmed123@gmail.com>
2025-08-10 07:47:10 +0000

    Convertion - Operations

---
+++ 01_basics/03_type_convertion_operations.js
@@ -0,0 +1,57 @@
+
+// Convertions---->
+
+// let str_age = "12abc" -> NaN
+// let str_age = null -> 0
+// let str_age = true -> true:1;false:0
+// let str_age = Boolean -> NaN
+let str_age = "12"
+
+// console.log(typeof(str_age));
+
+let val_age = Number(str_age)
+
+// console.log(typeof val_age);
+// console.log(val_age);
+
+
+// Operations ---->
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// str Operations
+
+// let str1 = "high"
+// let str2 = " school"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2*2);
+// console.log("1"+2+2);
+// console.log("1"+2-2);
+// console.log(1+2+"2");
+
+// Increment Operator
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
