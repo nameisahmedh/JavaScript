b26a69ffce4ef95d0317d61bfc4e8e3d03f80572
Md Ahmed <mdqamarahmed123@gmail.com>
2025-08-10 06:37:55 +0000

    var_let_const added

---
+++ 01_basics/01_var_const_let.js
@@ -0,0 +1,15 @@
+var n = "Rahul"
+const accountId = 563921
+let salary = 250050
+let passsword
+var branch
+n = "Karthik" // -> we can modify var 
+
+// accountId = 15 -> we cannot change const
+salary = 60000 // -> we can modify let
+console.log(n);
+
+console.table([n,accountId,salary,passsword,branch])
+
+
+
