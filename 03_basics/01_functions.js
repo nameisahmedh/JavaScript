032e68712e660e4ef5611e2b8f0a6d0503beb103
Md Ahmed <mdqamarahmed123@gmail.com>
2025-08-11 07:03:12 +0000

    function basics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -21,7 +21,7 @@
 function add(a,b){ // parameter
     return a+b;
 }
-console.log(add(5,6)); // arguments
+// console.log(add(5,6)); // arguments
 
 function greeting(username){
     if(!username){ // (username===undefined)
@@ -32,6 +32,35 @@ function greeting(username){
 }
 
 // console.log(greeting("Andriew"));
-console.log(greeting());
+// console.log(greeting());
+
+
+
+function calculateSum(...num1){ // Here ... is called as rest and spread operator according to the situation we use it as rest or spread this ... used to return the value in the new array 
+    return num1
+}
+
+console.log(calculateSum(500,200,600,1000)); // Here we will be passing multiple values
+
+const course = {
+    coursename: "JS Master",
+    price:1299
+}
+
+function couseObj(anyObj){
+    return `The Couse name is ${anyObj.coursename} and the price is ${anyObj.price} rupees`
+}
+// console.log(couseObj(course));
+console.log(couseObj({
+    coursename:"DSA Master",price:4999
+})); //----> We can directly pass object to the function
+
+
+const score = [500,800,1200]
+
+function getSecondVal(getVal){
+    return score[1];
+}
+console.log(getSecondVal(score));
 
 
