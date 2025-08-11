bdd3136c0a9380988714af798556b0154890d0fe
Md Ahmed <mdqamarahmed123@gmail.com>
2025-08-11 12:31:27 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,6 +6,34 @@ if(1){ // block scope
     // var z = 5 // It will declare globally
 }
 
-console.log(x);
+// console.log(x);
 // console.log(y);
 // console.log(z);
+
+
+//nested function
+
+function outer(){
+    const name = "David"
+    function inner(){
+        const user = "Catrin"
+        // console.log(name); // David    as it is in the inner so it can access the parent properties too
+        
+    }
+    inner()
+    // console.log(user); ----> as user inside inner it is in its block scope
+}
+outer()
+
+
+
+// console.log(addOne(12)) // We can use before declaration
+function addOne(num){
+    return num+1
+}
+
+
+// console.log(addTwo(52)) // It will make an error in this type of function usage we cannot use before as function is assigned to someone
+const addTwo = function(num){
+    return num+2
+}
