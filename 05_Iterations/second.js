36cb43f9afee8fd94455a2598c4e96edff040971
Md Ahmed <mdqamarahmed123@gmail.com>
2025-08-12 16:38:00 +0000

    while do-while

---
+++ 05_Iterations/second.js
@@ -0,0 +1,32 @@
+// while loop
+
+let index = 0
+while (index<10) {
+    console.log(index);
+    index++;
+}
+
+// initialisation
+// while(condition){
+//     // Print if while condition is true
+       // incre/decre
+// }
+
+
+
+// do while
+
+let i=1
+do {
+    console.log(i);
+    i++;
+
+} while (i<15);
+
+
+// let i=1
+// do {
+//     console.log(i); // It will execute atleast once
+//     i++;
+
+// } while (i<1);  
